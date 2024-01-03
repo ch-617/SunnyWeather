c1ca228a4d76fda2553b60a001acc347e7ef2b81
ch-617 <547823828@qq.com>
2024-01-03 12:27:17 +0800

    实现搜索全球城市数据功能

--- app/src/main/java/com/ihunuo/sunnyweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/ihunuo/sunnyweather/ui/place/PlaceFragment.kt
@@ -61,7 +61,7 @@ class PlaceFragment : Fragment() {
                 }
             }
         })
-        viewModel.placeLiveData.observe(this, Observer { result ->
+        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer { result ->
             val places = result.getOrNull()
             if (places != null) {
                 recyclerView.visibility = View.VISIBLE
